bce7c0737b5a6fc65d7e5a968e3e299cab1c671b
Shawn Koski <skoski@artnet.com>
2021-07-14 15:05:41 -0400

    change message from app script ui

--- Code.js
+++ Code.js
@@ -3,7 +3,7 @@
  */
 function createAndSendDocument() {
   // Create a new Google Doc named 'Hello, world!'
-  var doc = DocumentApp.create('Hello, from github!');
+  var doc = DocumentApp.create('Hello, in app script!');
 
   // Access the body of the document, then add a paragraph.
   doc.getBody().appendParagraph('This document was created by Google Apps Script.');
